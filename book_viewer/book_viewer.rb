1c89b5e237b9898ecf7faca7083a8b29787a22a1
dchae <daniel.j.chae@gmail.com>
2023-08-27 18:28:04 -0700

    starting dynamic directory index

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,6 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "tilt/erubis"
+
 get "/" do
-  File.read "public/template.html"
+  @contents = File.readlines("data/toc.txt")
+  @page_title = "Home"
+  erb :home #, locals: { page_title: "Page Title" }
+end
+
+get "/chapter-1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter= File.read("data/chp1.txt")
+
+  erb :chapter
+end
+
+get /.+/ do
+  "<h3>404 Page Not Found</h3>"
 end
