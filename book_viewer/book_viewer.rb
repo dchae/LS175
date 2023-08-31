d4f21d8a0bb2f66ac3e36fe66573192eab00f57a
dchae <daniel.j.chae@gmail.com>
2023-08-30 19:33:35 -0700

    added layout and modified home & chapter views

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,22 +1,28 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
 
-require "tilt/erubis"
+require 'tilt/erubis'
 
-get "/" do
-  @contents = File.readlines("data/toc.txt")
-  @page_title = "Home"
+get '/' do
+  @contents = File.readlines('data/toc.txt')
+  @page_title = 'Home'
   erb :home #, locals: { page_title: "Page Title" }
 end
 
-get "/chapter-1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter= File.read("data/chp1.txt")
+get '/chapters/:number' do |n|
+  @contents = File.readlines('data/toc.txt')
+  @title = "Chapter #{n}: #{@contents[n.to_i - 1]}"
+  @page_title = @title
+  @chapter = File.read("data/chp#{n}.txt")
 
   erb :chapter
 end
 
+get '/show/:name' do |name|
+  # name
+  params[:name]
+end
+
 get /.+/ do
-  "<h3>404 Page Not Found</h3>"
+  '<h3>404 Page Not Found</h3>'
 end
